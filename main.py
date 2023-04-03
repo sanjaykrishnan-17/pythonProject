32986fb0fef2207921a76fd383bd6c9804672665
rsa3001 <sanjaykrishnan13710@hmail.com>
2023-04-03 20:26:11 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Hello World")
-print("byee byeee")
-Print("after git in remote")
-print("Tata BYE BYE")
+import matplotlib
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+oo=pd.read_csv(r"C:\Users\rsa3001\PycharmProjects\pythonProject\ExerciseFiles\data\olympics.csv",skiprows=4)
+oo.head()
+ee=oo.dropna
+oo.Medal.value_counts()
+sns.countplot(x='Sport',data=oo)
+ee.info()
